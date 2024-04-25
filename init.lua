3e85eb325e0710b9cfc0be25699da0eafd764a5c
9qq-qq-qq9 <bladee@outlook.de>
2024-04-25 22:39:09 +0000

    inital commit

---
+++ init.lua
@@ -0,0 +1,18 @@
+-- runs our vim options on startup
+require("options")
+
+-- ensure lazy (our package manager) is installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
---
+++ lua/plugins.lua
@@ -0,0 +1,4 @@
+-- Lazy automatically imports all plugins/*.lua 
+-- and combines return values into the plugin table
+-- so no need to return anything here
+return {}
