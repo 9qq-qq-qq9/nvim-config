c2d72d260ddb25ed586924a699719dd9ab013387
9qq-qq-qq9 <bladee@outlook.de>
2024-04-26 18:46:09 +0000

    [*] adjust ibl char and remove underline

--- lua/plugins/ibl.lua
+++ lua/plugins/ibl.lua
@@ -2,6 +2,14 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
   config = function()
-    require("ibl").setup()
+    require("ibl").setup({
+      indent = {
+        char = "|"
+      },
+      scope = {
+        show_start = false,
+        show_end = false
+      }
+    })
   end
 }
