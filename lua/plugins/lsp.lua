5d79e5c11e2a80bbf554162969aea07f8166c1a1
9qq-qq-qq9 <bladee@outlook.de>
2024-04-27 21:22:40 -0700

    [*] adjust LSP setup to be more clean

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,27 +6,19 @@ return {
     end
   },
   {
+    "neovim/nvim-lspconfig",
+  },
+  {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "rust_analyzer" }
+        ensure_installed = { "lua_ls", "rust_analyzer" },
+        handlers = {
+          function(server)
+            require("lspconfig")[server].setup({})
+          end
+        }
       })
     end
   },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      require("mason-lspconfig").setup_handlers({
-        function(server)
-          lspconfig[server].setup({})
-        end,
-      })
-
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end
-  }
 }
