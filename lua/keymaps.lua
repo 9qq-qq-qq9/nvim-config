2f9332b4422403352ad8f8d10344735b190fdfdb
9qq-qq-qq9 <bladee@outlook.de>
2024-04-28 18:09:25 -0700

    [+] add none-ls plugin

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,1 @@
 vim.g.mapleader = " "
-
-
-vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
