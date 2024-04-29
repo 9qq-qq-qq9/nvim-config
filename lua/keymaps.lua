5b5201d2ac05fe08a696543f429f8e39d2f85c40
9qq-qq-qq9 <bladee@outlook.de>
2024-04-29 18:14:55 +0000

    [*] move buffer controls to keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,6 @@
 vim.g.mapleader = " "
 
+-- buffer control
 vim.keymap.set("n", "<Tab>", "<CMD>bnext<CR>", {})
 vim.keymap.set("n", "<S-Tab>", "<CMD>bprev<CR>", {})
+vim.keymap.set("n", "<leader>x", "<CMD>bdelete<CR>", {})
