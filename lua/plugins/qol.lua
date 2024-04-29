53e5cd39967ff5e96b7d12fca03b0c1469211981
9qq-qq-qq9 <bladee@outlook.de>
2024-04-29 18:05:08 +0000

    [-] remove which-key plugin

--- lua/plugins/qol.lua
+++ lua/plugins/qol.lua
@@ -14,12 +14,4 @@ return {
       require("Comment").setup()
     end,
   },
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-    end,
-  },
 }
