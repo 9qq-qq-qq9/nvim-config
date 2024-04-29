cb62e3697775db89572254ed320728f9b9e1d3cc
9qq-qq-qq9 <bladee@outlook.de>
2024-04-29 17:50:44 +0000

    [*] format entire project using stylua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,4 @@
--- Lazy automatically imports all plugins/*.lua 
+-- Lazy automatically imports all plugins/*.lua
 -- and combines return values into the plugin table
 -- so no need to return anything here
 return {}
--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,14 +1,14 @@
 return {
-  "rebelot/kanagawa.nvim",
-  lazy = false,
-  name = "kanagawa",
-  priority = 1000,
-  config = function()
-    require("kanagawa").setup({
-      commentStyle = { italic = false },
-      keywordStyle = { italic = false }
-    })
-    
-    vim.cmd.colorscheme("kanagawa")
-  end
+	"rebelot/kanagawa.nvim",
+	lazy = false,
+	name = "kanagawa",
+	priority = 1000,
+	config = function()
+		require("kanagawa").setup({
+			commentStyle = { italic = false },
+			keywordStyle = { italic = false },
+		})
+
+		vim.cmd.colorscheme("kanagawa")
+	end,
 }
