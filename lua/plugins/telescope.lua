478159a613f566353a7b5965661a77b7afbd56a9
9qq-qq-qq9 <bladee@outlook.de>
2024-05-01 16:03:22 +0000

    [*] change telescope keymapping

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -16,9 +16,9 @@ return {
       })
 
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
+      vim.keymap.set("n", "<C-f>", builtin.current_buffer_fuzzy_find, {})
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
       require("telescope").load_extension("ui-select")
     end,
