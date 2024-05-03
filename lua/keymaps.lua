de5562bf2a5bee00472ce15b3bd2c3c5fed29fca
9qq-qq-qq9 <bladee@outlook.de>
2024-05-03 15:36:22 +0000

    [+] new keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,5 +6,7 @@ vim.keymap.set("n", "<S-Tab>", "<CMD>bprev<CR>", {})
 vim.keymap.set("n", "<leader>x", "<CMD>bdelete<CR>", {})
 vim.keymap.set("n", "<leader>e", "<CMD>tabnew<CR>", {})
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {})
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {})
+
+vim.keymap.set("n", "<Esc>", "<CMD>noh<CR><Esc>", {})
