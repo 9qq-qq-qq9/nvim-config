050d0fc37df8f257230a5fb741cf2190958ba9b7
9qq-qq-qq9 <bladee@outlook.de>
2024-05-06 17:19:31 +0000

    [+] save file keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -10,3 +10,6 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {})
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {})
 
 vim.keymap.set("n", "<Esc>", "<CMD>noh<CR><Esc>", {})
+vim.keymap.set("i", "<S-Tab>", "<CMD> >> <CR>", {})
+
+vim.keymap.set("n", "ZW", "<CMD>w<CR>", {})
