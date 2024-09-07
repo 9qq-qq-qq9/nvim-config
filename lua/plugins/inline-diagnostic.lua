7785eeaae651eefc333614721d3fa5f6eee643a0
9qq-qq-qq9 <bladee@outlook.de>
2024-09-07 15:21:54 -0700

    [+] add inline diagnostic plugin

---
+++ lua/plugins/inline-diagnostic.lua
@@ -0,0 +1,10 @@
+-- needs configuring
+-- return {
+--     "rachartier/tiny-inline-diagnostic.nvim",
+--     event = "VeryLazy",
+--     config = function()
+--       vim.diagnostic.config({ virtual_text = false })
+--       require('tiny-inline-diagnostic').setup()
+--     end
+-- }
+return {}
